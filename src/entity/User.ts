358357171835c5bb6ac8315ba859efdae26f158f
devminchan <minchan.dev@gmail.com>
2020-03-22 20:39:24 +0900

    User.ts 수정

--- src/entity/User.ts
+++ src/entity/User.ts
@@ -1,16 +1,25 @@
 import { Entity, PrimaryGeneratedColumn, Column } from 'typeorm';
+import { ValidationEntity } from './ValidationEntity';
+import { Length, Min, IsEmail } from 'class-validator';
 
 @Entity()
-export class User {
+export class User extends ValidationEntity {
   @PrimaryGeneratedColumn()
   id: number;
 
+  @Length(3)
   @Column()
   firstName: string;
 
+  @Length(3)
   @Column()
   lastName: string;
 
+  @Min(18)
   @Column()
   age: number;
+
+  @IsEmail()
+  @Column()
+  email: string;
 }
